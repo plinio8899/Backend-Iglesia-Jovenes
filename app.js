53ae6e954fec6e603632cfd464171b356e3d11df
plinio8899 <plinio8899@gmail.com>
2025-04-04 17:08:27 -0500

    Prueba de commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import bodyParser from 'body-parser';
-//Creando el enrutador en un modulo aparte
 const app = express()
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
