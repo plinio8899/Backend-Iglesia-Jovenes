b8e7958c087a69dd8074297367e124d46e8b1e46
plinio8899 <plinio8899@gmail.com>
2025-04-04 17:31:51 -0500

    prueba de commit 2

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ import bodyParser from 'body-parser';
 const app = express()
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 app.use(bodyParser.json())
 export default app;
